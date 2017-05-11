627849734f7fd72cb544433894d972359ef48bef
measematt <measematt@users.noreply.github.com>
2017-05-11 15:08:14 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,41 @@ This is our Grunt Wrapper, this tells node we are using grunt!
  */
 
 module.exports = function (grunt) { 
-	/*
+	grunt.loadNpmTasks('grunt-sass');
+}
+module.exports = function (grunt) { 
+	grunt.loadNpmTasks('grunt-sass');
+
+	grunt.initConfig({
+		sass: {
+			dist: {
+				src: 'src/sass/style.scss',
+				dest: 'dist/css/style.css'
+			}
+		}
+	});
+};
+
+module.exports = function (grunt) { 
+	grunt.loadNpmTasks('grunt-sass');
+
+	grunt.initConfig({
+		sass: {
+			dist: {
+				src: 'src/sass/style.scss',
+				dest: 'dist/css/style.css'
+			}
+		}
+	});
+
+	grunt.registerTask('default', [
+		'sass'
+	]);
+};
+
+
+module.exports = function (grunt) { 
+/*
 	Loading our config
 	 */
 	var config = grunt.file.readYAML('Gruntconfig.yml');
